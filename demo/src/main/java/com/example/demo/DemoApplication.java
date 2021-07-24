7385c36c9da418a6a3d7713a990018379ca4ee08
chuenjit.vis <chuenjit.vis@ngernturbo.com>
2021-07-24 07:49:58 +0000

    first commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+		GenerateIdService service = ctx.getBean(GenerateIdService.class);
+		System.out.println("===>" + service.counter);
+		service.counter++;
+	}
+
+}
---
+++ demo/src/main/java/com/example/demo/GenerateIdService.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerateIdService {
+
+    public int counter;
+
+    public String getId() {
+        return "XYZ7";
+    }
+}
